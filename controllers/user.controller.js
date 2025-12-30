e5cc29e752a8befca57ef77657e9214ae98a82b1
nilaw2017 <nilaw.manandhar2016@gmail.com>
2025-12-30 13:25:31 +0545

    [BE]:: Update user error

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,5 @@
 import prisma from "../lib/prisma.js";
+import bcrypt from "bcrypt"
 export const getUsers = async(req, res) =>{
     try{
         const users = await prisma.user.findMany();
@@ -21,16 +22,41 @@ export const getUser = async(req,res)=> {
     }
 }
 export const updateUser = async(req,res)=> {
-    try {
+    const id = req.params.id
+    const tokenUserId = req.userId;
+    const {password, avatar, ...inputs} = req.body;
 
+    if(id!=tokenUserId) {
+        return res.status(403).json({message: "Not recognised!"})
+    }
+    let updatedPassword = null;
+    try {
+        if(password) {
+            updatedPassword = await bcrypt.hash(password,10);
+        }
+        const updatedUser = await prisma.user.update({
+            where: {id},
+            data: {
+                ...inputs,
+                ...(updatedPassword && {password:updatedPassword}),
+                ...(avatar && {avatar})
+            },
+        })
+        const {password, ...other} = updatedUser;
+        console.log(other);
+        res.status(200).json(other)
     }catch(err) {
         console.log(err)
         res.status(500).json({message:"Failed to update user!"})
     }
 }
 export const deleteUser = async(req,res)=> {
+    const id = req.params.id
     try {
-
+        await prisma.user.delete({
+            where: id
+        })
+        res.status(200).json({message:"User deleted"});
     }catch(err){
         console.log(err)
         res.status(500).json({message:"Unable to delete user!"})
