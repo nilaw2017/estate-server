af2c02b0dba94cab12e109dac5dccb0626282cfd
nilaw2017 <nilaw.manandhar2016@gmail.com>
2026-01-10 14:37:20 +0545

    [BE]:: Implemented displaying of chat messages

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ import cookieParser from "cookie-parser";
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
 import userRoute from "./routes/user.route.js";
-
+import messageRoute from "./routes/message.route.js"
+import chatRoute from "./routes/chat.route.js"
 const app = express();
 
 app.use(
@@ -19,6 +20,8 @@ app.use(cookieParser());
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/users", userRoute);
+app.use("/api/messages", messageRoute);
+app.use("/api/chats", chatRoute);
 
 app.listen(8800, () => {
   console.log("Server is running on port 8800");
